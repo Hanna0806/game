f7c2bca02aa4bdf4adbd6a1491618520bc51be4c
Anna Galanova <anna.gal0806@gmail.com>
2024-06-03 21:25:43 +0300

    add css

---
+++ mail.js
@@ -0,0 +1,7 @@
+const startBtn = document.getElementById('start');
+const cards = document.querySelectorAll('.card');
+const timeControler = document.querySelector('.time-list');
+const modal = document.getElementById('modal');
+const board = document.getElementById('board');
+const time = document.getElementById('time');
+const scroEl = document.getElementById('score');
